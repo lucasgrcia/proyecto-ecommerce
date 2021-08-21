844d8b0580956c7cde810d1c0b0c5ec6287ef380
luccasgrcia <lucasg.cb94@gmail.com>
2021-08-21 18:14:35 -0300

    Entrega-1-jap
    Trabajo realizado de  entrega 1

--- js/login.js
+++ js/login.js
@@ -2,5 +2,16 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
+function validacion() {
+     let usuario = document.getElementById("inputUser");
+    let password = document.getElementById("inputPassword");
+    if ((usuario.value !== "") && (password.value !=="")) {
+        window.location.href = "home.html";
+} else {
+    alert("Completa los campos de información!")
+    }
+}
 
+
+   validacion();
 });
