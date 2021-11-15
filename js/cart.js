d984af7414718874cadefca21fc52cadc6520669
Lucas Garcia <76232346+lucasgrcia@users.noreply.github.com>
2021-11-14 23:49:07 -0300

    Entrega 7
    Eliminar articulo, Validaciones , Envios

--- js/cart.js
+++ js/cart.js
@@ -1,20 +1,36 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
 const USD = 40;
 const ENVIOS = { standar: 5, express: 7, premium: 15 };
 //Función que se ejecuta una vez que se haya lanzado el evento de    
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e) {
+    //obtengo json de cart_full para mostrarlo 
     getJSONData(CART_FULL).then(cart => {
+        //si el resultado es exitoso
         if (cart.status === 'ok') {
+            //muestro la data de los articulos en el carrito
             articles = cart.data.articles;
             showCarrito(articles);
+            //obtengo id cotizacion
             document.getElementById('cotizacion').innerHTML = USD;
         }
     });
+    //Si obtengo una respuesta positiva, si la compra fue exitosa
+    if (localStorage.getItem('compraExitosa') === 'true') {
+        localStorage.removeItem('compraExitosa');
+        //muestro el mensaje de exito
+        document.getElementById('compraExitosa').innerHTML += `
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            <strong>¡Todo salio bien!</strong> Su compra se realizo exitosamente.
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        `;
+    }
 });
+
+
 //_____________________________________________________________________________________________-
 //FUNCION MOSTRAR CARRITO
 function showCarrito(cart) {
@@ -24,6 +40,7 @@ function showCarrito(cart) {
         <tr onchange="update(event)"> 
             <th scope="col" class="${'prod'+i}"><img src="${cart[i].src}" width="100px"></th>
             <th scope="col" class="${'prod'+i}">${cart[i].name}</th>
+            <button class="btn btn-danger" onclick="eliminarArticulo(0);">Eliminar</button>
             <th scope="col" class="${'prod'+i}">${cart[i].currency} <span class="cost">${cart[i].unitCost}</span></th>
             <th scope="col" class="${'prod'+i}"><input type="number" min="1" value="${cart[i].count}" data-class="${'prod'+i}"></th>
             <th scope="col" class="${'prod'+i}">${cart[i].currency} <span class="subTotal" data-currency="${cart[i].currency}" data-subtotal="${cart[i].unitCost * cart[i].count}">${cart[i].unitCost * cart[i].count}</span></th>
@@ -33,7 +50,17 @@ function showCarrito(cart) {
     document.getElementById('products').innerHTML = htmlContentToAppend;
     resumenUpdate();
 }
+
 //______________________________________________________________________________________________
+//Funcion para eliminar un articulo del carrito
+var articulo = [];
+function eliminarArticulo(id) { 
+    
+    let articulo = document.getElementsByClassName('prod'+id);
+    articulo[0].parentNode.remove();
+    resumenUpdate();
+}
+
 //Funcion que se encarga de sumar el precio de los productos en el subtotal
 function update(event) {
     dataProd = document.getElementsByClassName(event.target.dataset.class);
@@ -70,4 +97,34 @@ function resumenUpdate() {
     document.getElementById('env').innerHTML = pEnvio;
     //Seteo total
     document.getElementById('total').innerHTML = subtotal + pEnvio;
+}
+
+//Entrega 7
+//funcion para realizar la compra
+function pagar(nameForm) {
+    let form = document.getElementById(nameForm);
+    let error = '';
+    let warning = document.getElementById('warning');
+    // bucle para validar que todos los campos esten llenos en los formularios
+    for (let input of document.getElementsByClassName(nameForm + 'req')) {
+        // si el campo esta vacio
+        if (input.value === '') {
+            //muestro en consola el error si el campo esta vacio
+            console.log(input.dataset.campo);
+            //muestro el mensaje de error
+            error += '<p> Complete el campo ' + input.dataset.campo + '</p>';
+        }
+    }
+    //si hay algun error
+    warning.innerHTML = error;
+    if (error === '') {
+        //si no hay errores, se procede a realizar la compra
+        form.reset();
+        console.log('exito');
+        localStorage.setItem('compraExitosa', 'true');
+        // return true;
+    } else {
+        warning.hidden = false;
+        return false;
+    }
 }
